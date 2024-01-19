4e11151f47c16cc31f194ce8e572da0629ccbedd
Daveram <daveram4529a@gmail.com>
2024-01-18 20:02:17 -0800

    Imagedection.py

---
+++ Imagedection.py
@@ -0,0 +1,51 @@
+import imutils
+import cv2
+
+
+redLower = (15,164,162)
+redUpper =(45,232,255)
+
+camera =cv2.VideoCapture(0)
+
+while True:
+    (grabbed,frame) = camera.read() #reading frame from camera
+
+    frame = imutils.resize(frame,width=600)#resizing the frame
+    blurred = cv2.GaussianBlur(frame,(11,11),0)#smoooting
+    hsv  = cv2.cvtColor(blurred,cv2.COLOR_BGR2HSV)#convert bgr to hsv color formate
+
+    mask = cv2.inRange(hsv,redLower,redUpper) #mask the green color
+    mask = cv2.erode(mask,None,iterations=2)
+    mask=cv2.dilate(mask,None,iterations=2)
+
+    cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+   
+    center = None
+    if len(cnts)>0:
+            c=max(cnts,key=cv2.contourArea)
+            ((x,y),radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"]/M["m10"]),int(M["m10"]/M["m10"]))
+            if radius>10:
+                  cv2.circle(frame,(int (x),int(y)),int(radius),(0,255,255),2)
+                  cv2.circle(frame,center,5,(0,0,255),-1)
+
+                  if radius>250:
+                        print("Stop")
+                  else:
+                        if(center[0]<150):
+                              print("left")
+                        elif(center[0]>450):
+                              print("right")
+                        elif(radius<250):
+                              print("frount")
+                        else:
+                              print("Stop")
+
+    cv2.imshow("frame",frame)
+    key=cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+            break
+    
+camera.release()
+cv2.destroyAllWindows()
